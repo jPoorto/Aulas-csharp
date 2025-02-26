4b92110a094f36a4ab33f225dbd38b2657ac79cd
jPoorto <joao.ppgpardo@senacsp.edu.br>
2025-02-26 15:46:58 -0300

    Adicionar arquivos de projeto.

---
+++ Aula01/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Aula01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int idade = 19;
+            string nomeCompleto = "João Pedro Porto Gonçalves Pardo";
+
+            var variavelSemTipo = "João";
+
+            Console.WriteLine(variavelSemTipo.GetType());
+            Console.WriteLine("Digite seu nome: ");
+            Console.WriteLine("Digite sua idade: ");
+            string nomeUsuario = Console.ReadLine();
+
+
+
+
+
+
+            Console.WriteLine($"Meu nome é {nomeCompleto} e a minha idade é {idade} ");
+
+
+
+        }
+    }
+}
